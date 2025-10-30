310e4e18b7bf284d5e6091649f19055581459d21
taavitaht <taavi656@gmail.com>
2025-10-30 21:54:57 +0200

    repository

---
+++ src/main/java/ee/taavi/rendipood/repository/FilmRepository.java
@@ -0,0 +1,8 @@
+package ee.taavi.rendipood.repository;
+
+import ee.taavi.rendipood.entity.Film;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmRepository extends JpaRepository <Film>{
+
+}
