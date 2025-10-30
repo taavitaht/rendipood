d5f559c5b4a32b4ae2154758bca85c94f90b9a8a
taavitaht <taavi656@gmail.com>
2025-10-30 23:17:21 +0200

    edit and delete films

--- src/main/java/ee/taavi/rendipood/controller/FilmController.java
+++ src/main/java/ee/taavi/rendipood/controller/FilmController.java
@@ -3,10 +3,10 @@ package ee.taavi.rendipood.controller;
 import ee.taavi.rendipood.entity.Film;
 import ee.taavi.rendipood.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FilmController {
@@ -14,14 +14,42 @@ public class FilmController {
     @Autowired
     private FilmRepository filmRepository;
 
-    @GetMapping("allfilms")
-    public List<Film> getAllFilms(){
-        return filmRepository.findAll();
-    }
+    // Available films
     @GetMapping("availablefilms")
     public List<Film> getAvailableFilms(){
         return filmRepository.findByAvailableTrue();
     }
 
+    // All films
+    @GetMapping("films")
+    public List<Film> getAllFilms(){
+        return filmRepository.findAll();
+    }
+
+    // Add film
+    @PostMapping("films")
+    public List<Film> addFilm(@RequestBody Film film){
+        filmRepository.save(film);
+        return filmRepository.findAll();
+    }
+
+    // Delete film
+    // localhost:8080/films/1
+    @DeleteMapping("films/{id}")
+    public List<Film> removeFilm(@PathVariable Long id){
+        filmRepository.deleteById(id);
+        return filmRepository.findAll();
+    }
+
+    // Change film type, Patch request with text body
+    // localhost:8080/films/1
+    @PatchMapping("films/{id}")
+    public List<Film> patchFilm(@PathVariable Long id, @RequestBody String newType) {
+        Optional<Film> filmOptional = filmRepository.findById(id);
+        Film film = filmOptional.get();
+        film.setType(newType);
+        filmRepository.save(film);
+        return filmRepository.findAll();
+    }
 
 }
